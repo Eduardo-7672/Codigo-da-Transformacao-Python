f26aa0b3d0ef9ad8e23a0f5dc3f50cca3b29f3d8
Eduardo em Vocacao <eduardomd1668@gmail.com>
2025-10-13 17:09:31 -0300

    modulo update

---
+++ modulo_01/hello_world.py
@@ -0,0 +1,16 @@
+'''
+nomeclatura
+PASCALCase
+CamelCase
+-> Snake_Case <-
+Kebab-Case
+
+Comentários, pode ser usados como apostrofos
+ou cerquilhas
+instruções, expressõesx exibições em tela.
+'''
+# 
+#print ('Hello World')
+print ('Hello, World!')
+
+input ("Muito bem, qual o seu nome? ")
