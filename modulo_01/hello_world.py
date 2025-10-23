f631604e9c1d11faa68d9ec0585062171d3d730d
Eduardo em Vocacao <eduardomd1668@gmail.com>
2025-10-23 17:43:05 -0300

    Modulo update 2
    Atividade de CDT (inclusão de data, hora e mensagem receptiva)

--- modulo_01/hello_world.py
+++ modulo_01/hello_world.py
@@ -9,8 +9,26 @@ Comentários, pode ser usados como apostrofos
 ou cerquilhas
 instruções, expressõesx exibições em tela.
 '''
-# 
-#print ('Hello World')
+
 print ('Hello, World!')
 
-input ("Muito bem, qual o seu nome? ")
+nome_pessoa = str (input ("Qual o seu nome? "))
+print(f"Olá, {nome_pessoa}. Que nome bonito o seu!! ")
+
+import datetime
+
+# Obtém a data e hora atuais
+agora = datetime.datetime.now()
+
+# Formata a data e hora para exibição
+# %d - dia do mês
+# %m - mês
+# %Y - ano com 4 dígitos
+# %H - hora (formato 24h)
+# %M - minuto
+# %S - segundo
+data_hora_formatada = agora.strftime("%d/%m/%Y %H:%M:%S")
+
+# Imprime o resultado
+print(f"Seja bem vindo! Data e Hora Exatas: {data_hora_formatada}")
+	
